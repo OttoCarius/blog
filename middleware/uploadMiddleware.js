5b0b5ca2f9d7a3a037ef59f942ef7da0361eb6b0
OttoCarius <ottocarius1993@gmail.com>
2023-02-23 18:46:40 +0200

    fx

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -6,9 +6,9 @@ import multer from "multer";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_KEY,
-  api_secret: process.env.CLOUDINARY_SECRET,
+  cloud_name: "dfre3w1wc",
+  api_key: "581696524138893",
+  api_secret: "K1_PLDD6PgQ5RkQ33rBPeCGjYK8",
 });
 
 const storage = new CloudinaryStorage({
